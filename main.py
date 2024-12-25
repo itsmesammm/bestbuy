e79b541c2ffce16419a3e672c738e3a9e235b79f
Sam Millows <sammillows@sams-air.localdomain>
2024-12-26 00:54:37 +0100

    create Product class

---
+++ main.py
@@ -0,0 +1,19 @@
+from products import Product
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.show())
+print(mac.show())
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+print(bose.show())
+print(mac.show())
+
+print(bose.set_quantity(1000))
+print(bose.show())
+
+print(bose.get_quantity())
